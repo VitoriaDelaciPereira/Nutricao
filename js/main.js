4f15aff178f57f65f8ab1f44f07e3c684399edc4
VitoriaDelaciPereira <164279805+VitoriaDelaciPereira@users.noreply.github.com>
2024-11-19 22:42:22 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Vitória Nutrição";
+
+var titulo = document.querySelector("h2");
+titulo.textContent = "Pacientes Cadastrados";
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
